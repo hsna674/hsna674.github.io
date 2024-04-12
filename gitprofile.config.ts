2a66764172424c693f07171c971c831e191c266d
Ansh Agrawal <96297836+hsna674@users.noreply.github.com>
2024-04-12 08:32:36 -0400

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -57,23 +57,23 @@ const CONFIG = {
     imageURL: '',
   },
   social: {
-    linkedin: 'ariful-alam',
-    twitter: 'arif_szn',
-    mastodon: 'arifszn@mastodon.social',
+    linkedin: '',
+    twitter: '',
+    mastodon: '',
     researchGate: '',
     facebook: '',
     instagram: '',
     youtube: '', // example: 'pewdiepie'
     dribbble: '',
     behance: '',
-    medium: 'arifszn',
-    dev: 'arifszn',
+    medium: '',
+    dev: '',
     stackoverflow: '', // example: '1/jeff-atwood'
     skype: '',
     telegram: '',
-    website: 'https://www.arifszn.com',
+    website: '',
     phone: '',
-    email: 'arifulalamszn@gmail.com',
+    email: '',
   },
   resume: {
     fileUrl:
@@ -96,6 +96,7 @@ const CONFIG = {
     'Tailwind',
   ],
   experiences: [
+    /*
     {
       company: 'Company Name',
       position: 'Position',
@@ -110,30 +111,35 @@ const CONFIG = {
       to: 'August 2021',
       companyLink: 'https://example.com',
     },
+    */
   ],
   certifications: [
+    /*
     {
       name: 'Lorem ipsum',
       body: 'Lorem ipsum dolor sit amet',
       year: 'March 2022',
       link: 'https://example.com',
     },
+    */
   ],
   educations: [
     {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2015',
-      to: '2019',
-    },
+      institution: 'Thomas Jefferson High school for Science and Technology',
+      /*degree: 'Degree',*/
+      from: '2023',
+      to: '2027',
+    }/*,
     {
       institution: 'Institution Name',
       degree: 'Degree',
       from: '2012',
       to: '2014',
     },
+    */
   ],
   publications: [
+    /*
     {
       title: 'Publication Title',
       conferenceName: '',
@@ -152,20 +158,27 @@ const CONFIG = {
       description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     },
+    */
   ],
   // Display articles from your medium or dev account. (Optional)
   blog: {
+    /*
     source: 'dev', // medium | dev
     username: 'arifszn', // to hide blog section, keep it empty
     limit: 2, // How many articles to display. Max is 10.
+    */
   },
   googleAnalytics: {
+    /*
     id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
+    */
   },
   // Track visitor interaction and behavior. https://www.hotjar.com
   hotjar: {
+    /*
     id: '',
     snippetVersion: 6,
+    */
   },
   themeConfig: {
     defaultTheme: 'lofi',
